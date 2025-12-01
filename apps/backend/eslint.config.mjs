55f75be74644e0677a2fdcc15c01ba0fbe479b02
Ivan Shevchenko <sevcenkoivan673@gmail.com>
2025-12-01 17:14:34 +0200

    init monorepo

---
+++ apps/backend/eslint.config.mjs
@@ -0,0 +1,25 @@
+// @ts-check
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import rootConfig from '../../eslint.config.mjs';
+
+export default tseslint.config(
+  ...rootConfig,
+  {
+    ignores: ['eslint.config.mjs'],
+  },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+      ecmaVersion: 5,
+      sourceType: 'module',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  },
+);
---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+// @ts-check
+import eslint from '@eslint/js';
+import eslintPluginPrettierRecommended from 'eslint-plugin-prettier/recommended';
+import tseslint from 'typescript-eslint';
+import globals from 'globals';
+
+
+export default tseslint.config(
+  {
+    ignores: ['**/.turbo/**', '**/dist/**', 'eslint.config.mjs'],
+  },
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  eslintPluginPrettierRecommended,
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+      },
+      ecmaVersion: 5,
+      sourceType: 'module',
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+  }
+);
