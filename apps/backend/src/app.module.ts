94d539c487cbf0e8fb249de9d028d356eaf0fff2
Ivan Shevchenko <sevcenkoivan673@gmail.com>
2025-12-02 13:23:29 +0200

    chore(backend/auth): init auth module and install auth deps

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
+import { AuthService } from './auth/auth.service';
+import { AuthController } from './auth/auth.controller';
+import { AuthModule } from './auth/auth.module';
 import 'dotenv/config';
 
 @Module({
@@ -9,8 +12,9 @@ import 'dotenv/config';
       isGlobal: true,
     }),
     PrismaModule,
+    AuthModule,
   ],
-  controllers: [],
-  providers: [],
+  controllers: [AuthController],
+  providers: [AuthService],
 })
 export class AppModule {}
