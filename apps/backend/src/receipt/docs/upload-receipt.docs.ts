08231e96c66b7dd05ce6e3fd28e99f01220ac76b
Ivan Shevchenko <sevcenkoivan673@gmail.com>
2025-12-09 11:32:44 +0200

    docs(backend/receipt): write docs for ReceiptModule

---
+++ apps/backend/src/receipt/docs/upload-receipt.docs.ts
@@ -0,0 +1,66 @@
+import { applyDecorators } from '@nestjs/common';
+import {
+  ApiBadRequestResponse,
+  ApiBody,
+  ApiConsumes,
+  ApiOkResponse,
+  ApiOperation,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+
+export function UploadReceiptDocs() {
+  return applyDecorators(
+    ApiOperation({
+      summary: 'Upload receipt',
+      description: 'Upload receipt',
+    }),
+    ApiOkResponse({
+      description: 'Receipt upload successfully',
+      content: {
+        'application/json': {
+          example: {
+            url: 'https://buy-purchasing-powers-bucket.s3.eu-central-1.amazonaws.com/8f6d3c2b-4e71-4c8e-9b76-b2f1cc52c89e-01J0B9K9W9P2XKZ7VQF3ZC4TQS',
+          },
+        },
+      },
+    }),
+    ApiBadRequestResponse({
+      description: 'User already has pending receipt',
+      content: {
+        'application/json': {
+          example: {
+            message:
+              "You already have pending receipt! You can't create more than one pending receipt.",
+            error: 'Bad Request',
+            statusCode: 400,
+          },
+        },
+      },
+    }),
+    ApiUnauthorizedResponse({
+      description: 'User unauthorized',
+      content: {
+        'application/json': {
+          example: {
+            message: 'Unauthorized',
+            statusCode: 401,
+          },
+        },
+      },
+    }),
+    ApiConsumes('multipart/form-data'),
+    ApiBody({
+      description: 'File image/pdf upload',
+      schema: {
+        type: 'object',
+        properties: {
+          file: {
+            type: 'string',
+            format: 'binary',
+          },
+        },
+        required: ['file'],
+      },
+    }),
+  );
+}
--- apps/backend/src/receipt/receipt.controller.ts
+++ apps/backend/src/receipt/receipt.controller.ts
@@ -10,12 +10,15 @@ import { ReceiptService } from './receipt.service';
 import { JwtGuard } from '../auth/guards';
 import { UserDto } from '../common';
 import { User } from '../user/decorators';
+import { UploadReceiptDocs } from './docs';
 
 @UseGuards(JwtGuard)
 @Controller('receipt')
 export class ReceiptController {
   constructor(private readonly receiptService: ReceiptService) {}
 
+  // TODO: handle no-file scenario
+  @UploadReceiptDocs()
   @Post('upload')
   @UseInterceptors(FileInterceptor('file'))
   uploadFile(@User() user: UserDto, @UploadedFile() file: Express.Multer.File) {
