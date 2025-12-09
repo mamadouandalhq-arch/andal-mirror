4e5b09596ded23b0cce66cfeeec3c6035113c577
Ivan Shevchenko <sevcenkoivan673@gmail.com>
2025-12-09 17:04:31 +0200

    fix(backend/config): implement config validation of new env variables

--- apps/backend/src/common/config/validation.schema.ts
+++ apps/backend/src/common/config/validation.schema.ts
@@ -15,4 +15,7 @@ export const validationSchema = Joi.object({
   AWS_S3_ACCESS_KEY: Joi.string().required(),
   AWS_S3_SECRET_KEY: Joi.string().required(),
   AWS_S3_BUCKET_NAME: Joi.string().required(),
+
+  SESSION_SECRET: Joi.string().required(),
+  FRONTEND_URL: Joi.string().uri().required(),
 });
